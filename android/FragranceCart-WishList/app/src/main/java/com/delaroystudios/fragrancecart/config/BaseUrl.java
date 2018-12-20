201e83f28f4cbd306076487c209c6e8b89f6203f
Khaled Sayed <contact@khaledsayed.com>
2018-12-20 16:31:04 +0200

    Remove duplicated directory

--- android/FragranceCart-WishList/app/src/main/java/com/delaroystudios/fragrancecart/config/BaseUrl.java
+++
@@ -1,9 +0,0 @@
-package com.delaroystudios.fragrancecart.config;
-
-/**
- * Created by hp on 12/13/2018.
- */
-
-public class BaseUrl {
-    public static final String baseImageUrl = "http://192.168.1.104:8080/";
-}
